b42541528f46a8f1413f64d8b1fbdea195259963
shchi-create <shipanoff@gmail.com>
2026-01-13 17:22:00 +0300

    auth.py

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient
 
 api_id = 33360120
-api_hash = a61d4ff80e78b4fe12331ed373d3b362
+api_hash = "a61d4ff80e78b4fe12331ed373d3b362"
 
 with TelegramClient("session", api_id, api_hash) as client:
     print("Session created")
