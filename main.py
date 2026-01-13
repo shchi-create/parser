4f1e740d64ab060957c7cdaf911410f8b6801424
shchi-create <shipanoff@gmail.com>
2026-01-13 18:41:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
+
 # ───── Настройка Flask ─────
 app = Flask(__name__)
 
